bbfb7a19cf3c179a15e9adc6206582476ee083df
Abror Nabiddinov <nabiddinovabror8@gmail.com>
2023-06-06 17:03:59 +0500

    user service

--- MyTelegramBot/Services/UserService.cs
+++ MyTelegramBot/Services/UserService.cs
@@ -1,18 +1,50 @@
-﻿using MyTelegramBot.Entity;
+﻿using MyTelegramBot.Data;
+using MyTelegramBot.Entity;
 
 namespace MyTelegramBot.Services
 {
     public class UserService
     {
-        public UserService()
+        private readonly BotDbContext _context;
+
+        public UserService(BotDbContext context)
         {
+            _context = context ?? throw new ArgumentNullException(nameof(BotDbContext));
+        }
+
+        public async Task<User> GetUserAsync(long? userId)
+        {
+            ArgumentNullException.ThrowIfNull((userId));
+            ArgumentNullException.ThrowIfNull(_context.Users);
+
+
+            return await _context.Users.FindAsync(userId);
 
         }
 
-        public async Task<User> GetUserAsync(long accountId)
+        public async Task<(bool isSucsess, string ErrorMessage)> UpdateLanguageCodeAsync(long? userId, string LanguageCode)
         {
-            return new User() { LanguageCode = "en" };
+            ArgumentNullException.ThrowIfNull(LanguageCode);
 
+            var user = await GetUserAsync(userId);
+
+            if (user is null)
+                return (false, "User not found");
+
+            user.LanguageCode = LanguageCode;
+            _context.Users.Update(user);
+            await _context.SaveChangesAsync();
+
+            return (true, null);
         }
+
+
+        public async Task<string?> GetLanguageCodeAsync(long? userId)
+        {
+            var user = await GetUserAsync(userId);
+
+            return user?.LanguageCode;
+        }
+
     }
 }
